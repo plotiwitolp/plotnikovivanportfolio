a7528dc466e4180db73ce0ce88fe963957eb4f62
plotiwitolp <plotiwitolp@gmail.com>
2023-04-25 16:26:11 +0400

    update

--- front-page.php
+++ front-page.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <div class="page">
     <div class="block-top section">
-
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur vitae animi dolore labore dolorum. Neque et ullam nam explicabo quisquam dolorum quaerat commodi excepturi sed, nemo dolore rerum laboriosam velit?
     </div>
     <div class="block-technology section">
-
+        Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: постоянное информационно-пропагандистское обеспечение нашей деятельности говорит о возможностях как самодостаточных, так и внешне зависимых концептуальных решений. В рамках спецификации современных стандартов, реплицированные с зарубежных источников, современные исследования лишь добавляют фракционных разногласий и подвергнуты целой серии независимых исследований. В своём стремлении повысить качество жизни, они забывают, что постоянный количественный рост и сфера нашей активности предопределяет высокую востребованность модели развития.
     </div>
     <div class="block-portfolio section">
 
--- functions.php
+++ functions.php
@@ -3,10 +3,12 @@
 add_action('wp_enqueue_scripts', 'theme_name_scripts');
 function theme_name_scripts()
 {
-    $version = '1.0.0';
+    $version = '1.0';
     wp_enqueue_style('css-main', get_stylesheet_uri());
+    wp_enqueue_style('css-fonts',  get_template_directory_uri() . '/assets/fonts/montserrat/stylesheet.css', array(), $version);
     wp_enqueue_style('css-custom',  get_template_directory_uri() . '/assets/css/custom.css', array(), $version);
-    wp_enqueue_script('js-custom', get_template_directory_uri() . '/assets/js/custom.js', array(), $version, true);
+    wp_enqueue_script('js-jquery', get_template_directory_uri() . '/assets/libs/jquery/jquery-3.6.0.min.js', array(), $version, true);
+    wp_enqueue_script('js-custom', get_template_directory_uri() . '/assets/js/custom.js', array('js-jquery'), $version, true);
 }
 
 add_action('after_setup_theme', 'theme_register_nav_menu');
